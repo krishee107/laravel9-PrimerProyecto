47c3f98f5ddedcd3b348ef4c0adbf24e0da9fbc9
krishee107 <krishee107@gmail.com>
2023-05-01 20:47:16 +0200

    Estructura del proyecto y primeras rutas

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Laravel</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Laravel</h1>
+                </div>
+            </div>
+        </div>
+</html>
